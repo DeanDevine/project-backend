0f9bcd39f889e04463cc7e602ae00616fd9ea5af
DeanDevine <deanrussondevine@gmail.com>
2023-08-02 12:49:20 +0100

    fix databases

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -3,86 +3,91 @@ const app = require("../server");
 
 //const mongoose = require("mongoose");
 
-const mongoose = require("../connection.js")
-const seed = require('../seed.js');
+const mongoose = require("../connection.js");
+const seed = require("../seed.js");
 
-const data = require("../data/test/index.js");   //Import the test data
-
+const data = require("../data/test/index.js"); //Import the test data
 
 beforeEach(() => {
-   return seed(data);
- });
+  return seed(data);
+});
 
- 
 afterAll(() => {
   return mongoose.connection.close();
 });
 
-describe ('POST /api/users',() => {
-  test ('201:should return a new user', () => {
-      const new_user = {username:'username_test',password:"abc123",
-                         first_name:'test',last_name:'user'};
-      return request(app)
-      .post('/api/users')
+describe("POST /api/users", () => {
+  test("201:should return a new user", () => {
+    const new_user = {
+      username: "username_test",
+      password: "abc123",
+      first_name: "test",
+      last_name: "user",
+    };
+    return request(app)
+      .post("/api/users")
       .send(new_user)
       .expect(201)
-      .then (({body}) => {
-          const {user} = body;
-          expect(user.username).toEqual("username_test");
-          expect(user.password).toEqual("abc123");
-          expect(user.first_name).toEqual("test");
-          expect(user.last_name).toEqual("user");
-          expect(user.coins).toEqual(100);
-      })
+      .then(({ body }) => {
+        const { user } = body;
+        expect(user.username).toEqual("username_test");
+        expect(user.password).toEqual("abc123");
+        expect(user.first_name).toEqual("test");
+        expect(user.last_name).toEqual("user");
+        expect(user.coins).toEqual(100);
+      });
   });
-})
+});
 
-describe ('PATCH /api/users/:username',() => {
-  test ('201:should return a updated user', () => {
-      const updatedUser = {password:"cpass001"};
-      return request(app)
-      .patch('/api/users/Steven')
+describe("PATCH /api/users/:username", () => {
+  test("200:should update a user by username and respond with that updated user", () => {
+    const updatedUser = {
+      first_name: "test",
+      last_name: "user",
+    };
+    return request(app)
+      .patch("/api/users/test_user1")
       .send(updatedUser)
       .expect(200)
-      .then (({body}) => {
-          const {user} = body;
-          expect(user.username).toEqual("Steven");
-          expect(user.password).toEqual("cpass001");
-          expect(user.first_name).toEqual("first");
-          expect(user.last_name).toEqual("last");
-          expect(user.coins).toEqual(100);
-      })
+      .then(({ body }) => {
+        const { user } = body;
+        expect(user.username).toEqual("test_user1");
+        expect(user.password).toEqual("password1");
+        expect(user.first_name).toEqual("test");
+        expect(user.last_name).toEqual("user");
+        expect(user.coins).toEqual(100);
+      });
   });
-})
+});
 
-describe ("GET /api/users", () => {
-  test ("200:should return all users", () => {
+describe("GET /api/users", () => {
+  test("200:should return all users", () => {
     return request(app)
       .get("/api/users")
       .expect(200)
       .then(({ body }) => {
-        const {users} = body
+        const { users } = body;
         expect(users).toHaveLength(5);
-        users.forEach(user => { 
-            expect(user).toHaveProperty("username",expect.any(String));
-            expect(user).toHaveProperty("password",expect.any(String));
-            expect(user).toHaveProperty("first_name",expect.any(String));
-            expect(user).toHaveProperty("last_name",expect.any(String));
-            expect(user).toHaveProperty("coins",expect.any(Number));
+        users.forEach((user) => {
+          expect(user).toHaveProperty("username", expect.any(String));
+          expect(user).toHaveProperty("password", expect.any(String));
+          expect(user).toHaveProperty("first_name", expect.any(String));
+          expect(user).toHaveProperty("last_name", expect.any(String));
+          expect(user).toHaveProperty("coins", expect.any(Number));
         });
       });
   });
-})
+});
 
-describe ("GET /api/users/:username", () => {
-  test ("200:should return a single user by username", () => {
+describe("GET /api/users/:username", () => {
+  test("200:should return a single user by username", () => {
     return request(app)
-      .get("/api/users/Dean")
+      .get("/api/users/test_user1")
       .expect(200)
       .then(({ body }) => {
-        const {user} = body
+        const { user } = body;
         expect(user).toHaveProperty("password", expect.any(String));
-        expect(user.username).toEqual("Dean");
+        expect(user.username).toEqual("test_user1");
       });
   });
 
@@ -94,25 +99,6 @@ describe ("GET /api/users/:username", () => {
         expect(body.message).toBe(
           "Error: User username_which_does_not_exist not found"
         );
-      });
-  });
-});
-
-describe("PATCH /api/users/:username", () => {
-  test("200:should update a user by username and respond with that updated user", () => {
-    const updatedUser = {
-      first_name: "Dean",
-      last_name: "Devine",
-    };
-    return request(app)
-      .patch("/api/users/Dean")
-      .send(updatedUser)
-      .expect(200)
-      .then(({ body }) => {
-        const { user } = body;
-        expect(user.username).toEqual("Dean");
-        expect(user.first_name).toEqual("Dean");
-        expect(user.last_name).toEqual("Devine");
       });
   });
 });
--- data/test/user-data.js
+++ data/test/user-data.js
@@ -1,34 +1,34 @@
 const seedUsers = [
-    {
-      username: "Dean",
-      password: "password1",
-      first_name: "first",
-      last_name: "last",
-    },
-    {
-      username: "Dennis",
-      password: "password2",
-      first_name: "first",
-      last_name: "last",
-    },
-    {
-      username: "Steven",
-      password: "password3",
-      first_name: "first",
-      last_name: "last",
-    },
-    {
-      username: "Hannah",
-      password: "password4",
-      first_name: "first",
-      last_name: "last",
-    },
-    {
-      username: "Lucy",
-      password: "password5",
-      first_name: "first",
-      last_name: "last",
-    },
-  ];
+  {
+    username: "test_user1",
+    password: "password1",
+    first_name: "first",
+    last_name: "last",
+  },
+  {
+    username: "test_user2",
+    password: "password2",
+    first_name: "first",
+    last_name: "last",
+  },
+  {
+    username: "test_user3",
+    password: "password3",
+    first_name: "first",
+    last_name: "last",
+  },
+  {
+    username: "test_user4",
+    password: "password4",
+    first_name: "first",
+    last_name: "last",
+  },
+  {
+    username: "test_user5",
+    password: "password5",
+    first_name: "first",
+    last_name: "last",
+  },
+];
 
   module.exports = seedUsers
